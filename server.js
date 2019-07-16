b5700aeccf02721ae119d3136c5d501be772edb4
John Gellert <gellertjm@gmail.com>
2019-07-16 17:29:58 -0500

    Leap year jest testing complete.

--- server.js
+++ server.js
@@ -12,4 +12,24 @@ const fizzBuzz = (param) => {
     return param;
 }
 
-module.exports = fizzBuzz;
+const isLeapYear = (param) => {
+    if(param % 4 === 0 && param % 100!== 0 || param % 400 === 0) {
+        return true;
+    }
+    return false;
+}
+
+const leapYear = year => {
+    if(number % 400 === 0) {
+        return true;
+    } else if (number % 100 === 0) {
+        return false;
+    } else if (number % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {fizzBuzz, isLeapYear};
+
--- server.test.js
+++ server.test.js
@@ -1,4 +1,5 @@
-const fizzBuzz = require('./server.js');
+const { fizzBuzz } = require('./server.js');
+const { isLeapYear } = require('./server.js');
 
 test('FizzBuzz returns "Fizz" when the param is divisible by 3', () => {
     expect(fizzBuzz(3)).toBe("Fizz");
@@ -14,4 +15,16 @@ test('FizzBuzz returns "FizzBuzz" when the param is divisible by 3 and 5', () =>
 
 test('FizzBuzz returns the param when the param is not divisible by 3 and 5', () => {
     expect(fizzBuzz(7)).toBe(7);
+});
+
+test('isLeapYear returns true if param is a leap lear year', () => {
+    expect(isLeapYear(2016)).toBe(true);
+});
+
+test('isLeapYear returns false if param is not a lear year', () => {
+    expect(isLeapYear(2019)).toBe(false);
+});
+
+test('isLeapYear returns false if param is not a lear year', () => {
+    expect(isLeapYear(2100)).toBe(false);
 });
